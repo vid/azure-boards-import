202bbe5bb3c8300c8a45727d5d47e54cc1efff97
Vid <vid_github@zooid.org>
2020-11-30 16:46:40 -0500

    testing deletion

--- src/import-workitems.ts
+++ src/import-workitems.ts
@@ -237,6 +237,7 @@ async function deleteImportedWorkItems(wi, area) {
     const cis = await wi.getWorkItems(res.workItems.map(r => r.id));
     const wis = cis.filter(w => w.fields['System.Title'].match(/\(SIC-\d+\)$/)).map(w => w.id);
     console.info('deleting workitems:', wis);
+    /*
     for (const r of wis) {
         console.info('delete', r);
         try {
@@ -245,4 +246,5 @@ async function deleteImportedWorkItems(wi, area) {
             console.error(e);
         }
     }
+    */
 }
